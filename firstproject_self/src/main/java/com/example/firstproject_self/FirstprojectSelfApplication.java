d7e1ea74fbbb0b9b3b472059712bafdcfef26425
최근면 <kunmyon.choi@sk.com>
2022-03-24 19:36:11 +0900

    Finished lesson 3

---
+++ firstproject_self/src/main/java/com/example/firstproject_self/FirstprojectSelfApplication.java
@@ -0,0 +1,11 @@
+package com.example.firstproject_self;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstprojectSelfApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FirstprojectSelfApplication.class, args);
+    }
+}
